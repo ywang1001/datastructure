924bf425acc8f8b92713068273676c75bec23220
ywang218 <wangyicong12345@gmail.com>
2020-04-26 15:54:36 -0500

    0426coding

---
+++ heap/index.js
@@ -0,0 +1,82 @@
+class MinHeap {
+    constructor(arr=[]) {
+        this.heap = [];
+        this.heapLength = arr.length;
+        
+        for (var i = arr.length - 1; i >= 0; i--) {
+            this.insert(el);          
+        }
+    }
+    
+    get length() {
+        return this.heapLength;
+    }
+    
+    peek() {
+       return this.heapLength > 0 ? this.heap[0] : null;
+    }
+    
+    pop() {
+        if (this.heapLength > 0) {
+            const minEl = this.heap[0];
+            this.heap[0] = this.heap[this.heapLength - 1];
+            this.heapLength -= 1;
+            this._percolateDown(0);
+            return minEl;
+        }
+        return null;
+    }
+    
+    insert(el) {
+        if (this.heapLength < this.heap.length) {
+            this.heap[this.heapLength] = el;
+        } else {
+            this.heap.push(el);
+        }
+        this.heapLength += 1;
+        this._percolateUp(this.heapLength - 1);
+    }
+    
+    _percolateUp(ind) {
+        var parent = Math.floor((ind - 1) / 2);
+        
+        if (parent >= 0 && this.heap[parent] > this.heap[ind]) {
+            var temp = this.heap[parent];
+            this.heap[parent] = this.heap[ind];
+            this.heap[ind] = temp;
+            
+            this._percolateUp(parent);
+        }
+    }
+    
+    _percolateDown(ind) {
+        var leftChild = ind * 2 + 1;
+        var rightChild = ind * 2 + 2;
+        var minIndex = ind;
+        
+        if (leftChild < this.heapLength && this.heap[leftChild] < this.heap[minIndex]) {
+            minIndex = leftChild;
+        }
+        if (rightChild < this.heapLength && this.heap[rightChild] < this.heap[minIndex]) {
+            minIndex = rightChild;
+        }
+        
+        if (minIndex != ind) {
+            var temp = this.heap[ind];
+            this.heap[ind] = this.heap[minIndex];
+            this.heap[minIndex] = temp;
+            
+            this._percolateDown(minIndex);
+        }
+        
+    }
+}
+
+let arr = [1,9,2,8,3,7,4,6,5];
+var minHeap = new MinHeap();
+arr.forEach(num => minHeap.insert(num));
+console.log(minHeap.peek());
+minHeap.pop();
+console.log(minHeap.peek());
+minHeap.insert(1);
+console.log(minHeap.peek());
--- problems/twopointer/kthLargestNumber.js
+++ problems/twopointer/kthLargestNumber.js
@@ -1,10 +1,89 @@
-function kthLargetNumber(arr, k) {
+var res;
+function kthSmallestNumber(arr, k) {
     if(!arr || arr.length < k) {
         return -1;
     }
+    quickSelect(arr, 0, arr.length - 1, k-1);
+    return arr[res];
+}
 
+function quickSelect(arr, left, right, target) {
+    let mid = partition(arr, left, right);
+    if(mid === target) {
+       res = mid;
+       return;
+    }else if(target < mid) {
+        quickSelect(arr, left, mid-1, target);
+    }else {
+        quickSelect(arr, mid+1, right, target);
+    }
+    
 }
 
 function partition(arr, left, right) {
-    swap(arr, )
+    let pivot = arr[right];
+    let start = left;
+    let end = right - 1;
+    while(start <= end) {         //一定是<=不然会漏掉
+        if(arr[start] <= pivot) {
+            start++;
+        }else if(arr[end] > pivot) {
+            end--;
+        }else {
+            swap(arr, start++, end--);
+        }
+    }
+    swap(arr, start, right);
+    return start;
 }
+
+function swap(arr, l, r) {
+    let tp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = tp;
+}
+
+let input = [1,9,2,8,3,7,4,6,5,133,-3,27];
+console.log(kthSmallestNumber(input, 9));
+let inp = [5,2,4,1,3,6,0];
+console.log(kthSmallestNumber(inp, 4));
+
+var res2;
+function kLargestNumber(nums, k) {
+   if(!nums || nums.length < k) {
+       return -1;
+   }
+   quick2(nums, 0, nums.length - 1, k-1);
+   return nums[res2];
+}
+
+function quick2(nums, l, r, target) {
+    let m = partition(nums, l, r);
+    if(m === target) {
+        res2 = m;
+        return;
+    }else if(m < target) {
+        quick2(nums, m+1, r, target);
+    }else {
+        quick2(nums, l, m-1, target);
+    }
+}
+
+function partition(nums, l, r) {
+    let start = l, end = r-1;
+    while(start <= end) {
+        if(nums[start] >= nums[r]) {
+            start++;
+        }else if(nums[end] < nums[r]) {
+            end--;
+        }else {
+            swap(nums, start++, end--);
+        }
+    }
+    swap(nums, start, r);
+    return start;
+}
+
+let inp1 = [5,2,4,1,3,6,0];
+console.log('new, ',kLargestNumber(inp1, 4));
+console.log(inp1);
